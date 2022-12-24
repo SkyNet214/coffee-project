70cd37f1c48c6cada2497e03d7416bc064ed3106
SkyNet214 <niklas20066@gmail.com>
2022-12-24 14:55:22 +0100

    fixed bad credentials error by using app password from now on and email now has subject

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from datetime import date
 import datetime
-from email import message
 from flask import Flask, request, render_template, url_for
 import json, smtplib, ssl, time
+from email.mime.text import MIMEText
 
-from werkzeug.datastructures import ContentRange
 
 app = Flask(__name__)
 
@@ -12,17 +11,14 @@ immortal_id = "2357" # id for debugging
 
 sender_email = "remote.coffee.website@gmail.com"
 receiver_email = "niklas20066@gmail.com"
-password = "3M@w/%7v]m~g.B|]:}u&5R-.D;P{=>&N"
+password2 = "3M@w/%7v]m~g.B|]:}u&5R-.D;P{=>&N"
+app_password = "qcfipoftzfmkiqsl"
 port = 465
 context = ssl.create_default_context()
-message_template = lambda remarks, time: f"""\
-Subject: New coffee request received
+server = smtplib.SMTP("smtp.gmail.com", 587)
+server.starttls()
+server.login(sender_email, app_password)
 
-request time: {time}
-
-remarks: {remarks}
-
-"""
 
 @app.route("/")
 def index():
@@ -40,15 +36,18 @@ def idlist():
     elif request.method == "POST":
         body = request.json
         if body["valid"] == False and body["id"] != immortal_id:
-            with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-                server.login(sender_email, password)
-                print(server.sendmail(sender_email, receiver_email, message_template(body["remarks"], datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))))
+            date_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            remarks = body["remarks"]
+            msg = MIMEText(f"New coffee request received!\n\n {date_time} \n\n {remarks}")
+            msg["Subject"] = "New coffee request received!"
+            msg["From"] = sender_email
+            msg["To"] = receiver_email
+            print(server.sendmail(sender_email, receiver_email, msg.as_string()))
         id_list = json.loads(open("id_list.json", "r").read())
         for i in id_list["id_list"]["ids"]:
             if i["id"] == body["id"] and i["id"] != immortal_id:
                 i["valid"] = body["valid"]
         open("id_list.json", "w").write(json.dumps(id_list))
         return json.dumps({"success": True})
-
 
 app.run(host="0.0.0.0")
