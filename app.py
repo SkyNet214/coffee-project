069af478a9144b4d61888991bfbc5d7a7a8b04f7
SkyNet214 <niklas20066@gmail.com>
2023-06-26 08:03:56 +0200

    instead of coupon codes you now have a username with a number of coffe coupons attached to it you can redeem

--- app.py
+++ app.py
@@ -6,9 +6,8 @@ from email.mime.text import MIMEText
 
 app = Flask(__name__)
 
-with open("valid_codes.json") as f:
-    valid_codes = json.load(f)
-immortal_id = "2357" # id for debugging
+filename = "users.json"
+admin = "Heisenberg" # user for debugging
 
 sender_email = "remote.coffee.website@gmail.com"
 receiver_email = "niklas20066@gmail.com"
@@ -16,37 +15,56 @@ password = "3M@w/%7v]m~g.B|]:}u&5R-.D;P{=>&N"
 app_password = "qcfipoftzfmkiqsl"
 port = 587
 
+def read_file():
+    with open(filename, "r") as f:
+        return json.load(f)
+
+def write_file(username, count):
+    with open(filename, "w+") as f:
+        db = json.load(f)
+        if username in db.keys():
+            db[username] = count
+            json.dump(db, f)
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@app.route("/checkname", methods=["POST"])
+def check():
+    username = request.get_data(as_text=True)
+    db = read_file()
+    if username in db.keys():
+        return {"username": username, "count": db[username]} 
+    return {}
+
 @app.route("/submit", methods=["POST"])
 def submit():
-    code = request.form["code"]
+    username = request.form["username"]
     context = request.form["context"]
-    
-    if code in valid_codes["codes"]:
-        if code != immortal_id:
-            date_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            msg = MIMEText(f"New coffee request received!\n\n {date_time} \n\n {context}")
-            msg["Subject"] = "New coffee request received!"
-            msg["From"] = sender_email
-            msg["To"] = receiver_email
-            context = ssl.create_default_context()
-            server = smtplib.SMTP("smtp.gmail.com", port)
-            server.starttls()
-            server.login(sender_email, app_password)
-            print(server.sendmail(sender_email, receiver_email, msg.as_string()))
-            server.close()
-            valid_codes["codes"].remove(code)
-            with open("valid_codes.json", "w") as f:
-                json.dump(valid_codes, f)
+    db = read_file()
+    if username in db.keys():
+        if username != admin:
+            if db[username] <= 0:
+                return {"username": username, "count": -1}
+            else:
+                date_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                msg = MIMEText(f"New coffee request received from {username}!\n\n {date_time} \n\n {context}")
+                msg["Subject"] = "New coffee request received!"
+                msg["From"] = sender_email
+                msg["To"] = receiver_email
+                context = ssl.create_default_context()
+                server = smtplib.SMTP("smtp.gmail.com", port)
+                server.starttls()
+                server.login(sender_email, app_password)
+                print(server.sendmail(sender_email, receiver_email, msg.as_string()))
+                server.close()
+                db[username] = db[username] - 1
+                write_file(username, db[username])
+                return {"username": username, "count": db[username]}
         
-        return {"success": True}
-
-    else:
-        return {"success": False}
+        return {"username": username, "count": db[username]}
+    return {}
     
 
 app.run(host="0.0.0.0", port=80)
--- static/js/main.js
+++ static/js/main.js
@@ -1,47 +1,75 @@
 const form = document.getElementById("form");
+const message = document.getElementById("error-message");
+const input = document.getElementById("username-input");
+const result = document.getElementById("result");
+const counter = document.getElementById("counter");
+const count = document.getElementById("count");
+const button = document.getElementById("submit")
 
-// document.getElementById("code-input").setCustomValidity("Please enter a 4-digit code!");
 
 form.addEventListener("submit", (event) => {
     // Prevent the from from being submitted the default way
     event.preventDefault();
 
     // Send the fetch request
-    fetch("/submit", {
+    if (input.className === "valid")
+        fetch("/submit", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            body: `username=${encodeURIComponent(form.elements.username.value)}&context=${encodeURIComponent(form.elements.context.value)}`
+        })
+            .then(response => response.json())
+            .then(responseJSON => {
+                console.log(responseJSON)
+                const element = document.getElementById("result");
+                const error = document.getElementById("error-message");
+                const input = document.getElementById("username-input")
+                if (responseJSON.count < 0) {
+                    element.innerHTML = '<i class="failure">&#10007;</i> Failure';
+                    element.className = 'failure';
+                    error.innerHTML = "Invalid username";
+                    error.style.display = "block";
+                    input.className = "invalid";
+                } else {
+                    element.innerHTML = '<i class="success">&#10003;</i> Success';
+                    element.className = 'success';
+                    form.elements.context.value = "";
+                    count.innerHTML = responseJSON.count.toString();
+                }
+            })
+            .catch(error => console.error(error));
+});
+
+
+input.addEventListener("blur", () => {
+    counter.style.display = "none";
+    fetch("/checkname", {
         method: "POST",
         headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
+            "Content-Type": "text/plain"
         },
-        body: `code=${encodeURIComponent(form.elements.code.value)}&context=${encodeURIComponent(form.elements.context.value)}`
+        body: `${form.elements.username.value}`
     })
         .then(response => response.json())
         .then(responseJSON => {
-            console.log(responseJSON)
-            const element = document.getElementById("result");
-            const error = document.getElementById("error-message");
-            const input = document.getElementById("code-input")
-            if (responseJSON.success) {
-            
-                element.innerHTML = '<i class="success">&#10003;</i> Success';
-                element.className = 'success';
-                form.elements.code.value = "";
-                form.elements.context.value = "";
-            } else {
-                element.innerHTML = '<i class="failure">&#10007;</i> Failure';
-                element.className = 'failure';
-                error.innerHTML = "Invalid code";
-                error.style.display = "block";
+            if (Object.keys(responseJSON).length === 0 && responseJSON.constructor === Object) {
                 input.className = "invalid";
+                message.style.display = "block";
+                message.innerHTML = "Please enter a valid username"
+
+                
+            } else {
+                counter.style.display = "block";
+                count.innerHTML = responseJSON.count.toString();
+                input.className = "valid";
+                message.style.display = "none";
+                result.innerHTML = "";
             }
         })
-        .catch(error => console.error(error));
-});
-
-const message = document.getElementById("error-message");
-const input = document.getElementById("code-input");
-const result = document.getElementById("result");
-input.addEventListener("blur", () => {
-    if (input.checkValidity()) {
+    /*
+    if (input) {
         input.className = "valid";
         message.style.display = "none";
         result.innerHTML = "";
@@ -49,5 +77,5 @@ input.addEventListener("blur", () => {
         input.className = "invalid";
         message.style.display = "block";
         message.innerHTML = "Please enter a 4-digit code"
-    }
+    }*/
 });
